82902cfe36964b5f4949c0f096520874a1c2ad1b
andrew-sanders <137832043+andrew-sanders@users.noreply.github.com>
2023-06-30 16:01:10 -0600

    Undoing breaking changes

--- js/project2/main.js
+++ js/project2/main.js
@@ -2,17 +2,18 @@
     Code organization: 2/10
 */
 
-// import spells from "./scripts/spell_get";
+// import spells from "./spell_get";
 
 // API connection
 const API = "https://www.dnd5eapi.co/api";
 
-// Container
+// Elements 
 const container = document.querySelector(".container");
 
-// Page initialization
-let page = 1;
+// Class information
+// let wizardData = queryAPI("classes", "wizard");
 
+let page = 1;
 // Character Data
 let character = {
     name: null,
@@ -34,7 +35,43 @@ let character = {
     }
 }
 
-renderPageOne()
+function modifierCalc(val) {
+    if (val >= 30) {
+        return 10;
+    } else if (val >= 28) {
+        return 9;
+    } else if (val >= 26) {
+        return 8;
+    } else if (val >= 24) {
+        return 7;
+    } else if (val >= 22) {
+        return 6;
+    } else if (val >= 20) {
+        return 5;
+    } else if (val >= 18) {
+        return 4;
+    } else if (val >= 16) {
+        return 3;
+    } else if (val >= 14) {
+        return 2;
+    } else if (val >= 12) {
+        return 1;
+    } else if (val >= 10) {
+        return 0;
+    } else if (val >= 8) {
+        return -1;
+    } else if (val >= 6) {
+        return -2;
+    } else if (val >= 4) {
+        return -3;
+    } else if (val >= 2) {
+        return -4;
+    } else if (val === 1) {
+        return -5;
+    } else {
+        return null;
+    }
+}
 
 // Page advancer
 const left = document.querySelector("#left");
@@ -63,6 +100,43 @@ left.addEventListener("click", () => {
     page--;
     build(page);
 })
+
+renderPageOne()
+
+function build(page) {
+    container.innerHTML = "";
+    if (page === 1) {
+        renderPageOne();
+    } else if (page === 2) {
+        renderPageTwo();
+    } else if (page === 3) {
+        renderPageThree();
+    } else if (page === 4) {
+        renderPageFour();
+    } else if (page === 5) {
+        renderPageFive(spells);
+    } else if (page === 6) {
+        renderPageSix(spells);
+    } else {
+        container.innerHTML = "o.o you've entered the minus realm o.o";
+    }
+}
+
+// Arrow drag fix
+left.ondragstart = () => {
+    return false;
+};
+
+right.ondragstart = () => {
+    return false;
+}; 
+
+// Ability Querier
+async function queryAPI(section, subsection="") {
+    const apiReq = await fetch(`${API}/${section}/${subsection}`);
+    const apiData = await apiReq.json(); 
+    return apiData;
+}
 
 function renderPageOne() {
     container.innerHTML = `
