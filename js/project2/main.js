1f27a992945c6e2cf811de1ca355fb5c4ea630a8
andrew-sanders <137832043+andrew-sanders@users.noreply.github.com>
2023-06-30 16:38:26 -0600

    Fixed large blocks of code and removed unnecessary comments

--- js/project2/main.js
+++ js/project2/main.js
@@ -2,7 +2,8 @@
     Code organization: 2/10
 */
 
-// import spells from "./spell_get";
+import spells from "./scripts/spellGet.js";
+import modifierCalc from "./scripts/modifierCalc.js";
 
 // API connection
 const API = "https://www.dnd5eapi.co/api";
@@ -10,10 +11,6 @@ const API = "https://www.dnd5eapi.co/api";
 // Elements 
 const container = document.querySelector(".container");
 
-// Class information
-// let wizardData = queryAPI("classes", "wizard");
-
-let page = 1;
 // Character Data
 let character = {
     name: null,
@@ -35,43 +32,8 @@ let character = {
     }
 }
 
-function modifierCalc(val) {
-    if (val >= 30) {
-        return 10;
-    } else if (val >= 28) {
-        return 9;
-    } else if (val >= 26) {
-        return 8;
-    } else if (val >= 24) {
-        return 7;
-    } else if (val >= 22) {
-        return 6;
-    } else if (val >= 20) {
-        return 5;
-    } else if (val >= 18) {
-        return 4;
-    } else if (val >= 16) {
-        return 3;
-    } else if (val >= 14) {
-        return 2;
-    } else if (val >= 12) {
-        return 1;
-    } else if (val >= 10) {
-        return 0;
-    } else if (val >= 8) {
-        return -1;
-    } else if (val >= 6) {
-        return -2;
-    } else if (val >= 4) {
-        return -3;
-    } else if (val >= 2) {
-        return -4;
-    } else if (val === 1) {
-        return -5;
-    } else {
-        return null;
-    }
-}
+// Page initialization
+let page = 1;
 
 // Page advancer
 const left = document.querySelector("#left");
@@ -101,7 +63,15 @@ left.addEventListener("click", () => {
     build(page);
 })
 
-renderPageOne()
+// Arrow drag fix
+left.ondragstart = () => {
+    return false;
+};
+
+right.ondragstart = () => {
+    return false;
+}; 
+
 
 function build(page) {
     container.innerHTML = "";
@@ -122,14 +92,7 @@ function build(page) {
     }
 }
 
-// Arrow drag fix
-left.ondragstart = () => {
-    return false;
-};
-
-right.ondragstart = () => {
-    return false;
-}; 
+renderPageOne()
 
 // Ability Querier
 async function queryAPI(section, subsection="") {
@@ -210,156 +173,47 @@ function renderPageTwo() {
         </table>
         <p class="info"></p>
     `
-    // Better way to do this? Arrays and for loops probably
-    const chaVal = document.querySelector("#cha-value");
-    const conVal = document.querySelector("#con-value");
-    const dexVal = document.querySelector("#dex-value");
-    const intVal = document.querySelector("#int-value");
-    const strVal = document.querySelector("#str-value");
-    const wisVal = document.querySelector("#wis-value");
 
-    chaVal.innerHTML = character.ability_scores.cha;
-    conVal.innerHTML = character.ability_scores.con;
-    dexVal.innerHTML = character.ability_scores.dex;
-    intVal.innerHTML = character.ability_scores.int;
-    strVal.innerHTML = character.ability_scores.str;
-    wisVal.innerHTML = character.ability_scores.wis;
-
-    // And this? Most likely a method that takes two arguments, the skill and the increase or decrease
-    document.querySelector("#cha-dec").addEventListener("click", () => {
-        if (character.ability_scores.cha > 1) {
-            character.ability_scores.cha--;
-        } else {
-            alert("Ability scores cannot be reduced below 1. If they were, your character would be dead!")
-        }
-        chaVal.innerHTML = character.ability_scores.cha;
-    })
-    document.querySelector("#cha-inc").addEventListener("click", () => {
-        if (character.ability_scores.cha < 20) {
-            character.ability_scores.cha++;
-        } else {
-            alert("Ability scores cannot be higher than 20.")
-        }
-        chaVal.innerHTML = character.ability_scores.cha;
-    })
-
-    document.querySelector("#con-dec").addEventListener("click", () => {
-        if (character.ability_scores.con > 1) {
-            character.ability_scores.con--;
-        } else {
-            alert("Ability scores cannot be reduced below 1. If they were, your character would be dead!")
-        }
-        conVal.innerHTML = character.ability_scores.con;
-    })
-    document.querySelector("#con-inc").addEventListener("click", () => {
-        if (character.ability_scores.con < 20) {
-            character.ability_scores.con++;
-        } else {
-            alert("Ability scores cannot be higher than 20.")
-        }
-        conVal.innerHTML = character.ability_scores.con;
-    })
-
-    document.querySelector("#dex-dec").addEventListener("click", () => {
-        if (character.ability_scores.dex > 1) {
-            character.ability_scores.dex--;
-        } else {
-            alert("Ability scores cannot be reduced below 1. If they were, your character would be dead!")
-        }
-        dexVal.innerHTML = character.ability_scores.dex;
-    })
-    document.querySelector("#dex-inc").addEventListener("click", () => {
-        if (character.ability_scores.dex < 20) {
-            character.ability_scores.dex++;
-        } else {
-            alert("Ability scores cannot be higher than 20.")
-        }
-        dexVal.innerHTML = character.ability_scores.dex;
-    })
-
-    document.querySelector("#int-dec").addEventListener("click", () => {
-        if (character.ability_scores.int > 1) {
-            character.ability_scores.int--;
-        } else {
-            alert("Ability scores cannot be reduced below 1. If they were, your character would be dead!")
-        }
-        intVal.innerHTML = character.ability_scores.int;
-    })
-    document.querySelector("#int-inc").addEventListener("click", () => {
-        if (character.ability_scores.int < 20) {
-            character.ability_scores.int++;
-        } else {
-            alert("Ability scores cannot be higher than 20.")
-        }
-        intVal.innerHTML = character.ability_scores.int;
-    })
-
-    document.querySelector("#str-dec").addEventListener("click", () => {
-        if (character.ability_scores.str > 1) {
-            character.ability_scores.str--;
-        } else {
-            alert("Ability scores cannot be reduced below 1. If they were, your character would be dead!")
-        }
-        strVal.innerHTML = character.ability_scores.str;
-    })
-    document.querySelector("#str-inc").addEventListener("click", () => {
-        if (character.ability_scores.str < 20) {
-            character.ability_scores.str++;
-        } else {
-            alert("Ability scores cannot be higher than 20.")
-        }
-        strVal.innerHTML = character.ability_scores.str;
-    })
-
-    document.querySelector("#wis-dec").addEventListener("click", () => {
-        if (character.ability_scores.wis > 1) {
-            character.ability_scores.wis--;
-        } else {
-            alert("Ability scores cannot be reduced below 1. If they were, your character would be dead!")
-        }
-        wisVal.innerHTML = character.ability_scores.wis;
-    })
-    document.querySelector("#wis-inc").addEventListener("click", () => {
-        if (character.ability_scores.wis < 20) {
-            character.ability_scores.wis++;
-        } else {
-            alert("Ability scores cannot be higher than 20.")
-        }
-        wisVal.innerHTML = character.ability_scores.wis;
-    })
-
-    // Same here.
+    let stats = Object.keys(character.ability_scores);
     let abilityInfo = document.querySelector(".info");
-    document.querySelector("#cha h3").addEventListener("click", async () => {
-        let ability = await queryAPI("ability-scores", "cha");
-        console.log(Object.keys(ability));
-        abilityInfo.innerHTML = ability.desc[0] + " " + ability.desc[1];
-    })
 
-    document.querySelector("#con h3").addEventListener("click", async () => {
-        let ability = await queryAPI("ability-scores", "con");
-        abilityInfo.innerHTML = ability.desc[0] + " " + ability.desc[1];
-    })
-    
-    document.querySelector("#dex h3").addEventListener("click", async () => {
-        let ability = await queryAPI("ability-scores", "dex");
-        abilityInfo.innerHTML = ability.desc[0] + " " + ability.desc[1];
-    })
-    
-    document.querySelector("#int h3").addEventListener("click", async () => {
-        let ability = await queryAPI("ability-scores", "int");
-        abilityInfo.innerHTML = ability.desc[0] + " " + ability.desc[1];
-    })
-    
-    document.querySelector("#str h3").addEventListener("click", async () => {
-        let ability = await queryAPI("ability-scores", "str");
-        abilityInfo.innerHTML = ability.desc[0] + " " + ability.desc[1];
-    })
-    
-    document.querySelector("#wis h3").addEventListener("click", async () => {
-        let ability = await queryAPI("ability-scores", "wis");
-        abilityInfo.innerHTML = ability.desc[0] + " " + ability.desc[1];
-    })
+    function initializeStats(stat) {
+        document.querySelector(`#${stat}-value`).innerHTML = character.ability_scores[stat];
+    }
+
+    function valueChanger(stat) {
+        document.querySelector(`#${stat}-dec`).addEventListener("click", () => {
+            if (character.ability_scores[stat] > 1) {
+                character.ability_scores[stat]--;
+            } else {
+                alert("Ability scores cannot be reduced below 1. If they were, your character would be dead!")
+            }
+            document.querySelector(`#${stat}-value`).innerHTML = character.ability_scores[stat];
+        })
+        document.querySelector(`#${stat}-inc`).addEventListener("click", () => {
+            if (character.ability_scores[stat] < 20) {
+                character.ability_scores[stat]++;
+            } else {
+                alert("Ability scores cannot be higher than 20.")
+            }
+            document.querySelector(`#${stat}-value`).innerHTML = character.ability_scores[stat];
+        })
+        
+    }
+
+    async function loadStatInfo(stat) {
+        document.querySelector(`#${stat} h3`).addEventListener("click", async () => {
+            let ability = await queryAPI("ability-scores", `${stat}`);
+            console.log(Object.keys(ability));
+            abilityInfo.innerHTML = ability.desc[0] + " " + ability.desc[1];
+        })
+    }
+
+    for (let i = 0; i < stats.length; i++) {
+        initializeStats(stats[i]);
+        valueChanger(stats[i]);
+        loadStatInfo(stats[i]);
+    }
 }
 
 function renderPageThree() {
@@ -415,16 +269,19 @@ function renderPageFour() {
     </table>
     <p class="info"></p>
     `
-    container.style.justifyContent = "center"
+    container.style.justifyContent = "center";
+
     let profs = character.skills.length;
     let labels = document.querySelectorAll("label");
     let info = document.querySelector(".info");
+
     labels.forEach((item) => {
         item.addEventListener("click", async () => {
             let data = await queryAPI("skills", item.innerHTML.toLowerCase());
             info.innerHTML = `${data.desc[0]}`;
         })
     })
+
     let checkboxes = document.querySelectorAll(".checkbox");
     checkboxes.forEach((item) => {
         if (character.skills.includes(item.id)) {
@@ -440,8 +297,7 @@ function renderPageFour() {
                     }
                 }
                 character.skills.pop(); // leaves undefined value when popping with 1 element in the array
-                // clears undefined value
-                let filtered = character.skills.filter((x) => {
+                let filtered = character.skills.filter((x) => { // clears undefined value
                     return x !== undefined;
                 })
                 character.skills = filtered;
@@ -595,85 +451,76 @@ function renderPageSix(spells) {
     </table>
     `
     container.style.justifyContent = "center"
-    function loadChar() {
-        let nameEle = document.querySelector("#name");
-        let levelEle = document.querySelector("#level");
-        let classEle = document.querySelector("#class");
-        let hpEle = document.querySelector("#hp");
-        let chaEle = document.querySelector("#cha");
-        let conEle = document.querySelector("#con");
-        let dexEle = document.querySelector("#dex");
-        let intEle = document.querySelector("#int");
-        let strEle = document.querySelector("#str");
-        let wisEle = document.querySelector("#wis");
-        let skillsEle = document.querySelector("#skills");
-        let spellsEle = document.querySelector("#spells");
+    const nameEle = document.querySelector("#name");
+    const levelEle = document.querySelector("#level");
+    const classEle = document.querySelector("#class");
+    const hpEle = document.querySelector("#hp");
+    const chaEle = document.querySelector("#cha");
+    const conEle = document.querySelector("#con");
+    const dexEle = document.querySelector("#dex");
+    const intEle = document.querySelector("#int");
+    const strEle = document.querySelector("#str");
+    const wisEle = document.querySelector("#wis");
+    const skillsEle = document.querySelector("#skills");
+    const spellsEle = document.querySelector("#spells");
 
-        // fixes positive values to include '+' in string
-        let mods = [];
-        for (let i = 0; i < Object.values(character.ability_scores).length; i++) {
-            let mod = modifierCalc(Object.values(character.ability_scores)[i]);
-            if (mod >= 0) {
-                mod = `+${mod}`;
-            }
-            mods.push(mod);
+    // fixes positive values to include '+' in string
+    let mods = [];
+    for (let i = 0; i < Object.values(character.ability_scores).length; i++) {
+        let mod = modifierCalc(Object.values(character.ability_scores)[i]);
+        if (mod >= 0) {
+            mod = `+${mod}`;
         }
-
-        nameEle.innerHTML = character.name;
-        levelEle.innerHTML = character.level;
-        classEle.innerHTML = `${character.class.charAt(0).toUpperCase()}${character.class.slice(1)}`;
-        hpEle.innerHTML = character.hp;
-        chaEle.innerHTML = `${character.ability_scores.cha} (${mods[0]})`
-        conEle.innerHTML = `${character.ability_scores.con} (${mods[1]})`
-        dexEle.innerHTML = `${character.ability_scores.dex} (${mods[2]})`
-        intEle.innerHTML = `${character.ability_scores.int} (${mods[3]})`
-        strEle.innerHTML = `${character.ability_scores.str} (${mods[4]})`
-        wisEle.innerHTML = `${character.ability_scores.wis} (${mods[5]})`
-
-        skillsEle.innerHTML += "<tr>";
-        for (let i = 0; i < character.skills.length; i++) {
-            let skill = `${character.skills[i].charAt(0).toUpperCase()}${character.skills[i].slice(1)}`
-            skillsEle.innerHTML += `<td>${skill}</td>`
-        }
-        skillsEle.innerHTML += "</tr>";
-
-        async function renderSpells() {
-            for (let i = 0; i < spells.length; i++) {
-                if (character.spells[0].includes(spells[i].name) || character.spells[1].includes(spells[i].name)){
-                    spellsEle.innerHTML += `<tr><td>${spells[i].level}</td><td>${spells[i].name}</td><td class="school">${spells[i].school.name}</td></tr>`;  
-                }
-                          
-            }
-
-            // for (let i = 0; i < character.spells[1].length; i++) {
-            //     let name = character.spells[1][i].toLowerCase().replace(" ", "-");
-            //     let data = await queryAPI("spells", name);
-            //     spellsEle.innerHTML += `<tr><td>1</td><td>${data.name}</td><td class="school">${data.school.name}</td></tr>`;            
-            // }
-        }
-        renderSpells();
-
-        const schools = document.querySelectorAll(".school");
-        schools.forEach((item) => {
-            if (item.innerHTML === "Conjuration") {
-                item.style.color = "lightskyblue";
-            } else if (item.innerHTML === "Necromancy") {
-                item.style.color = "black";
-            } else if (item.innerHTML === "Evocation") {
-                item.style.color = "red";
-            } else if (item.innerHTML === "Transmutation") {
-                item.style.color = "gold";
-            } else if (item.innerHTML === "Illusion") {
-                item.style.color = "purple";
-            } else if (item.innerHTML === "Divination") {
-                item.style.color = "aqua";
-            } else if (item.innerHTML === "Enchantment") {
-                item.style.color = "yellow";
-            } else if (item.innerHTML === "Abjuration") {
-                item.style.color = "orange";
-            }
-        })
+        mods.push(mod);
     }
 
-    loadChar();
+    nameEle.innerHTML = character.name;
+    levelEle.innerHTML = character.level;
+    classEle.innerHTML = `${character.class.charAt(0).toUpperCase()}${character.class.slice(1)}`;
+    hpEle.innerHTML = character.hp;
+    chaEle.innerHTML = `${character.ability_scores.cha} (${mods[0]})`
+    conEle.innerHTML = `${character.ability_scores.con} (${mods[1]})`
+    dexEle.innerHTML = `${character.ability_scores.dex} (${mods[2]})`
+    intEle.innerHTML = `${character.ability_scores.int} (${mods[3]})`
+    strEle.innerHTML = `${character.ability_scores.str} (${mods[4]})`
+    wisEle.innerHTML = `${character.ability_scores.wis} (${mods[5]})`
+
+    skillsEle.innerHTML += "<tr>";
+    for (let i = 0; i < character.skills.length; i++) {
+        let skill = `${character.skills[i].charAt(0).toUpperCase()}${character.skills[i].slice(1)}`
+        skillsEle.innerHTML += `<td>${skill}</td>`
+    }
+    skillsEle.innerHTML += "</tr>";
+
+    async function renderSpells() {
+        for (let i = 0; i < spells.length; i++) {
+            if (character.spells[0].includes(spells[i].name) || character.spells[1].includes(spells[i].name)){
+                spellsEle.innerHTML += `<tr><td>${spells[i].level}</td><td>${spells[i].name}</td><td class="school">${spells[i].school.name}</td></tr>`;  
+            }
+                        
+        }
+    }
+
+    renderSpells();
+
+    const schools = document.querySelectorAll(".school");
+    schools.forEach((item) => {
+        if (item.innerHTML === "Conjuration") {
+            item.style.color = "lightskyblue";
+        } else if (item.innerHTML === "Necromancy") {
+            item.style.color = "black";
+        } else if (item.innerHTML === "Evocation") {
+            item.style.color = "red";
+        } else if (item.innerHTML === "Transmutation") {
+            item.style.color = "gold";
+        } else if (item.innerHTML === "Illusion") {
+            item.style.color = "purple";
+        } else if (item.innerHTML === "Divination") {
+            item.style.color = "aqua";
+        } else if (item.innerHTML === "Enchantment") {
+            item.style.color = "yellow";
+        } else if (item.innerHTML === "Abjuration") {
+            item.style.color = "orange";
+        }
+    })
 }
